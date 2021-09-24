1d51470e339f31b3df7c6d82627b4d996a24ce00
Cesar-in Angeles <cesar.angeles@hotmail.es>
2021-09-23 20:39:36 -0500

    adding files to be completed by student

---
+++ test/test_fibonacciSeries.c
@@ -0,0 +1,23 @@
+#define VALUES 6
+#define TEST_INDECES 1, 2, 3, 5, 6, 0 
+#define SERIES_RESULTS 1, 1, 2, 5, 8, 0
+
+void setUp(void){
+
+}
+
+void tearDown(void){
+
+}
+
+void test_arrayValues(void){
+    int test_indeces[VALUES] = {TEST_INDECES};
+    int test_results[VALUES] = {SERIES_RESULTS};
+    
+
+}
+
+
+int main(void){
+
+}
